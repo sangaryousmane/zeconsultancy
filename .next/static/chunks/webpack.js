c9806f4c1b8ac0fbc73669f3fc83228a1949d7c4
ousmane <sangaryousmane@gmail.com>
2025-08-27 11:33:58 +0000

    Remove ID for all services folders

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "59cbe8b1567165c3"; }
+/******/ 		__webpack_require__.h = function() { return "5ff9835742e97a3f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
